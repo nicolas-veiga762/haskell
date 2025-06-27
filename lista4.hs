3b3f7c3c636b6b4a9809ebd7fff568e4ebbbb351
Nicolas Oliveira Veiga <nicolasveiga24@gmail.com>
2025-06-26 21:59:31 -0300

    Update lista4.hs
    Signed-off-by: Nicolas Oliveira Veiga <nicolasveiga24@gmail.com>

--- lista4.hs
+++ lista4.hs
@@ -1,5 +1,9 @@
 -- funções para aprender: foldr, filter, zip, repeat, words, lines, all, map
 
+import Data.List (sortBy)
+
+--a função do proprio haskell
+
 -- b
 numLines xs = numLines' 1 xs
 numLines' _ [] = []
@@ -11,4 +15,4 @@ allNumWords ((n, x):xs) = zip (repeat n) (words x) ++ allNumWords xs
 
 --d
 sortLs [] = []
-sortLs = 
+sortLs x = sortBy (\(_, a) (_, b) -> compare a b) x
