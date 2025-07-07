449265030f266129955c66749afcf348fbb3a0b8
Nicolas Oliveira Veiga <nicolasveiga24@gmail.com>
2025-07-07 16:01:55 -0300

    Add files via upload
    Signed-off-by: Nicolas Oliveira Veiga <nicolasveiga24@gmail.com>

---
+++ nova_lista.hs
@@ -0,0 +1,61 @@
+-- Lista de Exercícios Funcionais em Haskell – Nova Lista
+
+-- 1. Retorna o maior entre três números
+maior3 :: Int -> Int -> Int -> Int
+maior3 a b c = undefined
+
+-- 2. Verifica se um número é par ou ímpar
+parImpar :: Int -> String
+parImpar n = undefined
+
+-- 3. Calcula o fatorial de um número
+fatorial :: Int -> Int
+fatorial n = undefined
+
+-- 4. Calcula o MDC usando o algoritmo de Euclides
+mdc :: Int -> Int -> Int
+mdc a b = undefined
+
+-- 5. Verifica se uma string é um palíndromo
+palindromo :: String -> Bool
+palindromo s = undefined
+
+-- 6. Conta quantas vezes um elemento aparece na lista
+contaElem :: Eq a => a -> [a] -> Int
+contaElem x xs = undefined
+
+-- 7. Dobra apenas os números pares da lista
+dobrarPares :: [Int] -> [Int]
+dobrarPares xs = undefined
+
+-- 8. Verifica se todos os elementos da lista são positivos
+todosPositivos :: [Int] -> Bool
+todosPositivos xs = undefined
+
+-- 9. Soma todos os múltiplos de 3 da lista
+somaMultiplosDe3 :: [Int] -> Int
+somaMultiplosDe3 xs = undefined
+
+-- 10. Insere um elemento em uma lista ordenada mantendo a ordem
+inserirOrdenado :: Int -> [Int] -> [Int]
+inserirOrdenado e xs = undefined
+
+-- 11. Converte todas letras minúsculas em maiúsculas
+maiusculas :: String -> String
+maiusculas s = undefined
+
+-- 12. Conta o número de vogais em uma string
+contaVogais :: String -> Int
+contaVogais s = undefined
+
+-- 13. Remove todos os espaços de uma string
+removeEspacos :: String -> String
+removeEspacos s = undefined
+
+-- 14. Inverte a ordem das palavras de uma frase
+reversoPalavras :: String -> String
+reversoPalavras s = undefined
+
+-- 15. Extrai apenas os números de uma string
+soNumeros :: String -> String
+soNumeros s = undefined
